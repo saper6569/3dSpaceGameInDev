067b17fa0979d1c01878122bb28591e72b79ce19
playper3 <saper6569@gmail.com>
2021-09-25 19:24:37 -0700

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,159 @@
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.plugins.ZipLocator;
+import com.jme3.audio.AudioData;
+import com.jme3.audio.AudioKey;
+import com.jme3.audio.AudioNode;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+
+public class main extends SimpleApplication implements ActionListener {
+
+    private Spatial sceneModel;
+    private BulletAppState bulletAppState;
+    private RigidBodyControl landscape;
+    private CharacterControl player;
+    private Vector3f walkDirection = new Vector3f();
+    private boolean left = false, right = false, up = false, down = false, notWalking = true;
+
+    private Vector3f camDir = new Vector3f();
+    private Vector3f camLeft = new Vector3f();
+
+    private AudioNode walkingSounds;
+
+    public static void main(String[] args) {
+        main app = new main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        initCrossHairs("+");
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
+        flyCam.setMoveSpeed(100);
+        setUpKeys();
+        setUpLight();
+
+        assetManager.registerLocator("town.zip", ZipLocator.class);
+        sceneModel = assetManager.loadModel("main.scene");
+        sceneModel.setLocalScale(2f);
+
+        CollisionShape sceneShape = CollisionShapeFactory.createMeshShape(sceneModel);
+        landscape = new RigidBodyControl(sceneShape, 0);
+        sceneModel.addControl(landscape);
+
+        CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+        player = new CharacterControl(capsuleShape, 0.05f);
+        player.setJumpSpeed(20);
+        player.setFallSpeed(30);
+
+        rootNode.attachChild(sceneModel);
+        bulletAppState.getPhysicsSpace().add(landscape);
+        bulletAppState.getPhysicsSpace().add(player);
+
+        player.setGravity(60);
+        player.setPhysicsLocation(new Vector3f(0, 10, 0));
+
+        initSound();
+    }
+
+    private void initSound() {
+        walkingSounds = new AudioNode(assetManager, "Sound/footsteps.wav", AudioData.DataType.Stream);
+        walkingSounds.setPositional(false);
+        walkingSounds.setLooping(false);
+    }
+
+    private void setUpLight() {
+        AmbientLight al = new AmbientLight();
+        al.setColor(ColorRGBA.White.mult(1.3f));
+        rootNode.addLight(al);
+
+        DirectionalLight dl = new DirectionalLight();
+        dl.setColor(ColorRGBA.White);
+        dl.setDirection(new Vector3f(2.8f, -2.8f, -2.8f).normalizeLocal());
+        rootNode.addLight(dl);
+    }
+
+    public void initCrossHairs(String crossHair) {
+        setDisplayStatView(false);
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        BitmapText ch = new BitmapText(guiFont, false);
+        ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
+        ch.setText(crossHair);
+        ch.setLocalTranslation( settings.getWidth() / 2 - ch.getLineWidth()/2, settings.getHeight() / 2 + ch.getLineHeight()/2, 0);
+        guiNode.attachChild(ch);
+    }
+
+    private void setUpKeys() {
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(this, "Left");
+        inputManager.addListener(this, "Right");
+        inputManager.addListener(this, "Up");
+        inputManager.addListener(this, "Down");
+        inputManager.addListener(this, "Jump");
+    }
+
+    public void onAction(String binding, boolean isPressed, float tpf) {
+        if (binding.equals("Left")) {
+            left = isPressed;
+        }
+        else if (binding.equals("Right")) {
+            right= isPressed;
+        }
+        else if (binding.equals("Up")) {
+            up = isPressed;
+        }
+        else if (binding.equals("Down")) {
+            down = isPressed;
+        }
+        else if (binding.equals("Jump") && player.onGround()) {
+            if (isPressed) {
+                player.jump();
+            }
+        }
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        camDir.set(cam.getDirection()).multLocal(0.3f);
+        camLeft.set(cam.getLeft()).multLocal(0.2f);
+        walkDirection.set(0, 0, 0);
+        if (left) {
+            walkDirection.addLocal(camLeft);
+
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+
+        }
+        if (up) {
+            walkDirection.addLocal(camDir);
+
+        }
+        if (down) {
+            walkDirection.addLocal(camDir.negate());
+        }
+
+        player.setWalkDirection(walkDirection);
+        cam.setLocation(player.getPhysicsLocation());
+    }
+}
+
