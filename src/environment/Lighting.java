d224517f38073091c1010027ba600443f2e224d8
playper3 <saper6569@gmail.com>
2021-09-26 20:42:45 -0700

    cleaned up lighting class

--- src/environment/Lighting.java
+++ src/environment/Lighting.java
@@ -1,5 +1,6 @@
 package environment;
 
+import com.jme3.app.SimpleApplication;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
@@ -7,7 +8,7 @@ import com.jme3.math.Vector3f;
 
 public class Lighting {
 
-    public static DirectionalLight setUpLight(Vector3f direction, com.jme3.math.ColorRGBA colour) {
+    public static DirectionalLight setUpLight (Vector3f direction, com.jme3.math.ColorRGBA colour) {
         DirectionalLight dl = new DirectionalLight();
         dl.setColor(colour);
         dl.setDirection(direction.normalizeLocal());
