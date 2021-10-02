b0812ba6ca832863e65c9f0fe20e5cc1c31b37ca
playper3 <saper6569@gmail.com>
2021-10-01 19:08:10 -0700

    created class for player creation and player activity

---
+++ src/game/player/Player.java
@@ -0,0 +1,44 @@
+package game.player;
+
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.math.Vector3f;
+import game.screens.MainScreenSetup;
+
+public class Player {
+
+    private static CharacterControl player;
+
+    private MainScreenSetup mainScreen;
+
+    public void initialize() {
+        CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+        player = new CharacterControl(capsuleShape, 0.05f);
+
+        player.setJumpSpeed(20);
+        player.setFallSpeed(30);
+        player.setGravity(60);
+        player.setPhysicsLocation(new Vector3f(0, 20, 0));
+        mainScreen = new MainScreenSetup();
+    }
+
+    public Object getPlayer() {
+        return player;
+    }
+
+    public static void jump() {
+        player.jump();
+    }
+
+    public static boolean isOnGround() {
+        return player.onGround();
+    }
+
+    public static void setWalkDirection(Vector3f walkDirection) {
+        player.setWalkDirection(walkDirection);
+    }
+
+    public static Vector3f getPhysicsLocation() {
+        return player.getPhysicsLocation();
+    }
+}
