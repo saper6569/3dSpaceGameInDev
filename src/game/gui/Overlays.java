0345fc3f72403fff16a7061e0d9b05269c9242da
playper3 <saper6569@gmail.com>
2021-10-01 19:23:31 -0700

    fixed crosshair and added getters

--- src/game/gui/Overlays.java
+++ src/game/gui/Overlays.java
@@ -4,11 +4,13 @@ import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.font.BitmapFont;
 import com.jme3.font.BitmapText;
 
 public class Overlays extends AbstractAppState {
 
     private SimpleApplication app;
+    private BitmapFont guiFont;
 
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
@@ -16,12 +18,11 @@ public class Overlays extends AbstractAppState {
         this.app = (SimpleApplication)app;
 
         this.app. setDisplayStatView(false);
-        this.app.
-        this.app.getGuiNode() = this.app.getAssetManager().loadFont("Interface/Fonts/Default.fnt");
-        BitmapText ch = new BitmapText(guiFont, false);
-        ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
-        ch.setText("X");
-        ch.setLocalTranslation( this.app.getCamera().getWidth() / 2 - ch.getLineWidth()/2,this.app.getCamera().getHeight() / 2 + ch.getLineHeight()/2, 0);
-        this.app.getGuiNode().attachChild(ch);
+        guiFont = this.app.getAssetManager().loadFont("Interface/Fonts/Default.fnt");
+        BitmapText cross = new BitmapText(guiFont, false);
+        cross.setSize(guiFont.getCharSet().getRenderedSize() * 2);
+        cross.setText("X");
+        cross.setLocalTranslation( this.app.getCamera().getWidth() / 2 - cross.getLineWidth()/2, this.app.getCamera().getHeight() / 2 + cross.getLineHeight()/2, 0);
+        this.app.getGuiNode().attachChild(cross);
     }
 }
