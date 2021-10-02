c85f864beb74189f72a1aed9924beb038370abdf
playper3 <saper6569@gmail.com>
2021-10-01 19:21:41 -0700

    cleaned up all unecessary methods

--- src/main.java
+++ src/main.java
@@ -1,23 +1,11 @@
 import com.jme3.app.SimpleApplication;
-import com.jme3.input.FlyByCamera;
-import com.jme3.input.InputManager;
-import com.jme3.input.KeyInput;
-import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.InputListener;
-import com.jme3.input.controls.KeyTrigger;
+import com.jme3.font.BitmapFont;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
 import game.screens.MainScreenSetup;
 
-class main extends SimpleApplication implements ActionListener {
+class main extends SimpleApplication {
 
     MainScreenSetup mainScreenSetup = new MainScreenSetup();
-
-    private Vector3f walkDirection = new Vector3f();
-    private boolean left = false, right = false, up = false, down = false, notWalking = true;
-
-    private Vector3f camDir = new Vector3f();
-    private Vector3f camLeft = new Vector3f();
 
     public static void main(String[] args) {
         main app = new main();
@@ -29,73 +17,10 @@ class main extends SimpleApplication implements ActionListener {
         stateManager.attach(mainScreenSetup);
         viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
         flyCam.setMoveSpeed(100);
-        setUpKeys();
     }
 
-    private void setUpKeys() {
-        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
-        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
-        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
-        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
-        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
-        inputManager.addListener(this, "Left");
-        inputManager.addListener(this, "Right");
-        inputManager.addListener(this, "Up");
-        inputManager.addListener(this, "Down");
-        inputManager.addListener(this, "Jump");
-    }
-
-    public void onAction(String binding, boolean isPressed, float tpf) {
-        if (binding.equals("Left")) {
-            left = isPressed;
-        }
-        else if (binding.equals("Right")) {
-            right= isPressed;
-        }
-        else if (binding.equals("Up")) {
-            up = isPressed;
-        }
-        else if (binding.equals("Down")) {
-            down = isPressed;
-        }
-        else if (binding.equals("Jump") && player.onGround()) {
-            if (isPressed) {
-                player.jump();
-            }
-        }
-    }
-
-    @Override
-    public void simpleUpdate(float tpf) {
-        camDir.set(cam.getDirection()).multLocal(0.3f);
-        camLeft.set(cam.getLeft()).multLocal(0.2f);
-        walkDirection.set(0, 0, 0);
-        if (left) {
-            walkDirection.addLocal(camLeft);
-
-        }
-        if (right) {
-            walkDirection.addLocal(camLeft.negate());
-
-        }
-        if (up) {
-            walkDirection.addLocal(camDir);
-
-        }
-        if (down) {
-            walkDirection.addLocal(camDir.negate());
-        }
-
-        player.setWalkDirection(walkDirection);
-        cam.setLocation(player.getPhysicsLocation());
-    }
-
-    public FlyByCamera getFlyCam () {
-        return flyCam;
-    }
-
-    public InputManager getInputManager () {
-        return inputManager;
+    public BitmapFont getFont() {
+        return guiFont;
     }
 }
 
