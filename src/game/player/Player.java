e79e34686c8a89152699a96804dbcc39494d78f1
playper3 <saper6569@gmail.com>
2021-10-03 19:15:39 -0700

    added torch mechanic

--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -1,25 +1,48 @@
 package game.player;
 
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
 import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
 import com.jme3.bullet.control.CharacterControl;
+import com.jme3.light.Light;
+import com.jme3.light.SpotLight;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.LightControl;
+import game.environment.Lighting;
 import game.screens.MainScreenSetup;
 
 public class Player {
 
     private static CharacterControl player;
+    private SpotLight torch;
 
-    private MainScreenSetup mainScreen;
-
-    public void initialize() {
+    public void initialize(Node node) {
         CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
         player = new CharacterControl(capsuleShape, 0.05f);
 
-        player.setJumpSpeed(20);
-        player.setFallSpeed(15);
+        player.setJumpSpeed(15);
+        player.setFallSpeed(10);
         player.setGravity(30);
         player.setPhysicsLocation(new Vector3f(0, 5, 0));
-        mainScreen = new MainScreenSetup();
+
+        torch = new SpotLight();
+        torch.setSpotRange(110f);
+        torch.setSpotInnerAngle(40f * FastMath.DEG_TO_RAD);
+        torch.setSpotOuterAngle(70f * FastMath.DEG_TO_RAD);
+        torch.setColor(ColorRGBA.White.mult(10f));
+        node.addLight(torch);
+    }
+
+    public void update(Camera camera) {
+        torch.setDirection(camera.getDirection());
+        torch.setPosition(camera.getLocation());
     }
 
     public Object getPlayer() {
--- src/game/screens/MainScreenSetup.java
+++ src/game/screens/MainScreenSetup.java
@@ -9,9 +9,16 @@ import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.light.PointLight;
+import com.jme3.light.SpotLight;
+import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.control.LightControl;
+import com.jme3.texture.Texture;
+import com.jme3.util.TangentBinormalGenerator;
 import game.environment.Lighting;
 import game.gui.Overlays;
 import game.player.Movement;
@@ -23,35 +30,40 @@ public class MainScreenSetup extends AbstractAppState {
     private Spatial sceneModel;
     private RigidBodyControl landscape;
     private BulletAppState bulletAppState;
-    private Player player;
+    private Player player = new Player();
+    private Material sceneMaterial;
 
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
         super.initialize(stateManager, app);
         this.app = (SimpleApplication)app;
+
         Movement movement = new Movement();
         Overlays overlays = new Overlays();
-        stateManager.attach(movement);
-        stateManager.attach(overlays);
-        player = new Player();
-        player.initialize();
 
-        this.app.getAssetManager().registerLocator("town.zip", ZipLocator.class);
-        sceneModel = this.app.getAssetManager().loadModel("main.scene");
-        sceneModel.setLocalScale(2f);
+        stateManager.attach(overlays);
+        player.initialize(this.app.getRootNode());
+        stateManager.attach(movement);
+
+        sceneModel = this.app.getAssetManager().loadModel("Models/starbase.gltf");
+        sceneModel.setLocalScale(2.5f);
+        this.app.getRootNode().attachChild(sceneModel);
 
         CollisionShape sceneShape = CollisionShapeFactory.createMeshShape(sceneModel);
         landscape = new RigidBodyControl(sceneShape, 0);
         sceneModel.addControl(landscape);
-        this.app.getRootNode().attachChild(sceneModel);
 
-        this.app.getRootNode().addLight(Lighting.setUpLight(ColorRGBA.White.mult(1.3f)));
-        this.app.getRootNode().addLight(Lighting.setUpLight(new Vector3f(2.8f, -2.8f, -2.8f), ColorRGBA.White));
+        this.app.getRootNode().addLight(Lighting.setUpLight(ColorRGBA.White.mult(10)));
 
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
         bulletAppState.getPhysicsSpace().add(landscape);
         bulletAppState.getPhysicsSpace().add(player.getPlayer());
+    }
+
+    @Override
+    public void update(float delta) {
+        player.update(this.app.getCamera());
     }
 
     @Override
