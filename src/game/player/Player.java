033155fb9d861e9d1536fce20faaa42cf7951ae0
playper3 <saper6569@gmail.com>
2021-11-15 17:05:05 -0800

    changes to player coords

--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -21,7 +21,6 @@ import game.screens.MainScreenSetup;
 public class Player {
 
     private static CharacterControl player;
-    private SpotLight torch;
 
     public void initialize(Node node) {
         CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
@@ -31,18 +30,6 @@ public class Player {
         player.setFallSpeed(10);
         player.setGravity(30);
         player.setPhysicsLocation(new Vector3f(0, 5, 0));
-
-        torch = new SpotLight();
-        torch.setSpotRange(110f);
-        torch.setSpotInnerAngle(40f * FastMath.DEG_TO_RAD);
-        torch.setSpotOuterAngle(70f * FastMath.DEG_TO_RAD);
-        torch.setColor(ColorRGBA.White.mult(10f));
-        node.addLight(torch);
-    }
-
-    public void update(Camera camera) {
-        torch.setDirection(camera.getDirection());
-        torch.setPosition(camera.getLocation());
     }
 
     public Object getPlayer() {
